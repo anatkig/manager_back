dd04afd8c60dcfac0eb9135a742430c8adcf74ad
Anatolii Kiguradze <anatoliikiguradze@Anatoliis-MacBook-Pro.local>
2023-10-18 12:47:45 +0600

    fix to the previous

--- Program.cs
+++ Program.cs
@@ -4,6 +4,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.Configure<StorageConfig>(builder.Configuration.GetSection("AzureTableStorage"));
+builder.Services.AddScoped<IStorageService, StorageService>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
